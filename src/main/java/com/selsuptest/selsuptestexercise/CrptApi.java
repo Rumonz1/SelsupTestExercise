8605e6b462ad78e511b39a2fda258462fcaf338a
Rumonz <lone_assassin@mail.ru>
2024-01-29 12:47:35 +0300

    finish Selsup test exercize

--- src/main/java/com/selsuptest/selsuptestexercise/CrptApi.java
+++ src/main/java/com/selsuptest/selsuptestexercise/CrptApi.java
@@ -1,130 +1,119 @@
 package com.selsuptest.selsuptestexercise;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 
-@Component
 public class CrptApi {
-    private final TimeUnit timeUnit;
-    private final int requestLimit;
-    private final AtomicInteger requestCount = new AtomicInteger(0);
-    private long lastTimeReset;
-    @Autowired
-    private RestTemplate restTemplate;
+
+    private final String apiUrl = "https://ismp.crpt.ru/api/v3/lk/documents/create";
+    private final HttpClient httpClient;
+    private final ObjectMapper objectMapper;
+    private final Semaphore requestSemaphore;
 
     public CrptApi(TimeUnit timeUnit, int requestLimit) {
-        this.timeUnit = timeUnit;
-        this.requestLimit = requestLimit;
-        this.lastTimeReset = System.currentTimeMillis();
+        this.httpClient = HttpClient.newHttpClient();
+        this.objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+        this.requestSemaphore = new Semaphore(requestLimit);
+        Runnable releasePermitTask = () -> {
+            try {
+                requestSemaphore.release();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(releasePermitTask, 0, timeUnit.toSeconds(1), TimeUnit.SECONDS);
     }
 
-    @Bean
-    public TimeUnit getTimeUnit() {
-        return timeUnit;
-    }
-    @Bean
-    public int getRequestLimit() {
-        return requestLimit;
-    }
-    @Bean
-    public RestTemplate getRestTemplate() {
-        return restTemplate;
+    public void createDocument(Document document, String signature) {
+        try {
+            requestSemaphore.acquire();
+            String documentJson = objectMapper.writeValueAsString(document);
+            ObjectNode requestBody = objectMapper.readValue(documentJson, ObjectNode.class);
+            requestBody.put("signature", signature);
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI(apiUrl))
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(requestBody.toString()))
+                    .build();
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() == 200) {
+                System.out.println("Документ успешно создан");
+            } else {
+                System.err.println("Ошибка при создании документа. HTTP Status Code: " + response.statusCode());
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        } finally {
+            requestSemaphore.release();
+        }
     }
 
 
-    @Data
-    private static class Document {
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class Description {
+        @JsonProperty("participantInn")
         private String participantInn;
-        private String docId;
-        private String docStatus;
-        private String docType;
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class Document {
+        private Description description;
+        private String doc_id;
+        private String doc_status;
+        private String doc_type;
         private boolean importRequest;
-        private String ownerInn;
-        private String producerInn;
-        private String productionDate;
-        private String productionType;
+        private String owner_inn;
+        private String participant_inn;
+        private String producer_inn;
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        private LocalDate production_date;
+        private String production_type;
         private List<Product> products;
-        private String regDate;
-        private String regNumber;
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        private LocalDate reg_date;
+        private String reg_number;
     }
 
-    @Data
-    private static class Product {
-        private String certificateDocument;
-        private String certificateDocumentDate;
-        private String certificateDocumentNumber;
-        private String ownerInn;
-        private String producerInn;
-        private String productionDate;
-        private String tnvedCode;
-        private String uitCode;
-        private String uituCode;
-    }
-
-    public synchronized void createDocument(Document document, String sign) {
-        refreshRequestCount();
-        if (requestCount.get() < requestLimit) {
-            String url = "https://ismp.crpt.ru/api/v3/lk/documents/create";
-            String requestBody = constructRequestBody(document, sign);
-            String response = restTemplate.postForObject(url, requestBody, String.class);
-            System.out.println("Document created and signed successfully.");
-            requestCount.incrementAndGet();
-        } else {
-            System.out.println("Request limit exceeded. Please try again later.");
-        }
-    }
-    private String constructRequestBody(Document document, String sign) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        ObjectNode requestBody = objectMapper.createObjectNode();
-        requestBody.put("participantInn", document.getParticipantInn());
-        requestBody.put("docId", document.getDocId());
-        requestBody.put("docStatus", document.getDocStatus());
-        requestBody.put("docType", document.getDocType());
-        requestBody.put("importRequest", document.isImportRequest());
-        requestBody.put("ownerInn", document.getOwnerInn());
-        requestBody.put("producerInn", document.getProducerInn());
-        requestBody.put("productionDate", document.getProductionDate());
-        requestBody.put("productionType", document.getProductionType());
-        requestBody.put("regDate", document.getRegDate());
-        requestBody.put("regNumber", document.getRegNumber());
-
-        ArrayNode productsArray = objectMapper.createArrayNode();
-        for (Product product : document.getProducts()) {
-            ObjectNode productNode = objectMapper.createObjectNode();
-            productNode.put("certificateDocument", product.getCertificateDocument());
-            productNode.put("certificateDocumentDate", product.getCertificateDocumentDate());
-            productNode.put("certificateDocumentNumber", product.getCertificateDocumentNumber());
-            productNode.put("ownerInn", product.getOwnerInn());
-            productNode.put("producerInn", product.getProducerInn());
-            productNode.put("productionDate", product.getProductionDate());
-            productNode.put("tnvedCode", product.getTnvedCode());
-            productNode.put("uitCode", product.getUitCode());
-            productNode.put("uituCode", product.getUituCode());
-            productsArray.add(productNode);
-        }
-        requestBody.set("products", productsArray);
-
-        requestBody.put("sign", sign);
-
-        return requestBody.toString();
-    }
-
-    private void refreshRequestCount() {
-        long currentTime = System.currentTimeMillis();
-        long passedTime = currentTime - lastTimeReset;
-        if (timeUnit.toMillis(1) <= passedTime) {
-            requestCount.set(0);
-            lastTimeReset = currentTime;
-        }
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class Product {
+        private String certificate_document;
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        private LocalDate certificate_document_date;
+        private String certificate_document_number;
+        private String owner_inn;
+        private String producer_inn;
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        private LocalDate production_date;
+        private String tnved_code;
+        private String uit_code;
+        private String uitu_code;
     }
 }
